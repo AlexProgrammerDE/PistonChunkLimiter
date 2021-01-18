45ebfabb78ee746437ef1d3da426424d3cee18cb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-01-18 19:49:14 +0100

    Minor fixes

--- src/main/java/net/pistonmaster/pistonchunklimiter/PistonChunkLimiter.java
+++ src/main/java/net/pistonmaster/pistonchunklimiter/PistonChunkLimiter.java
@@ -2,7 +2,7 @@ package net.pistonmaster.pistonchunklimiter;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Chunk;
-import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,7 +11,6 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PistonChunkLimiter extends JavaPlugin implements Listener {
-
     @Override
     public void onEnable() {
         getLogger().info(ChatColor.RED + "Loading config");
@@ -28,30 +27,31 @@ public final class PistonChunkLimiter extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         Block block = event.getBlockPlaced();
         Chunk chunk = block.getChunk();
+        int c = getConfig().getInt("chests-per-chunk");
+        int s = getConfig().getInt("shulkers-per-chunk");
 
-        if (block.getType() == Material.CHEST
-                && getAmountOf(chunk, "CHEST") >= getConfig().getInt("chests-per-chunk")) {
+        if (block.getType().toString().contains("CHEST")
+                && getAmountOf(chunk, "CHEST") >= c) {
             event.setBuild(false);
-            player.sendMessage(format(getConfig().getString("chest-message")));
+            player.sendMessage(format(getConfig().getString("chest-message").replace("%chests_per_chunk%", String.valueOf(c))));
         } else if (block.getType().toString().contains("SHULKER_BOX")
-                && getAmountOf(chunk, "SHULKER_BOX") >= getConfig().getInt("shulkers-per-chunk")) {
+                && getAmountOf(chunk, "SHULKER_BOX") >= s) {
             event.setBuild(false);
-            player.sendMessage(format(getConfig().getString("shulker-message")));
+            player.sendMessage(format(getConfig().getString("shulker-message").replace("%shulkers_per_chunk%", String.valueOf(s))));
         }
     }
 
     public static int getAmountOf(final Chunk chunk, final String material) {
-        final int minX = chunk.getX() << 4;
-        final int minZ = chunk.getZ() << 4;
-        final int maxX = minX | 15;
-        final int maxY = chunk.getWorld().getMaxHeight();
-        final int maxZ = minZ | 15;
         int amount = 0;
+        int bx = chunk.getX()<<4;
+        int bz = chunk.getZ()<<4;
 
-        for (int x = minX; x <= maxX; ++x) {
-            for (int y = 0; y <= maxY; ++y) {
-                for (int z = minZ; z <= maxZ; ++z) {
-                    if (chunk.getBlock(x, y, z).getType().toString().contains(material)) {
+        World world = chunk.getWorld();
+
+        for(int xx = bx; xx < bx+16; xx++) {
+            for(int zz = bz; zz < bz+16; zz++) {
+                for(int yy = 0; yy < 128; yy++) {
+                    if (world.getBlockAt(xx, yy, zz).getType().toString().contains(material)) {
                         ++amount;
                     }
                 }
